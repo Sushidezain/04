0bd822fb39e5ce905aa2a42900ee64a0ce0dbe6d
Sushidezain <136917380+Sushidezain@users.noreply.github.com>
2023-06-18 21:47:26 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -15,7 +15,7 @@ def main():
     # Set the locale for formatting thousands separator
     locale.setlocale(locale.LC_ALL, '')
 
-    EP0 = st.number_input('EP0', value=0.0, format="%.0f", step=1000.0)
+    EP0 = st.number_input('EP0', value=0.0, format="%.0f", step=1000.0, value=1000000)
     IP1 = st.slider('IP1', min_value=120.0, max_value=150.0, step=0.1, value=120.0)
     PM1 = st.slider('PM1', min_value=39000, max_value=50000, step=1000, value=40000)
 
