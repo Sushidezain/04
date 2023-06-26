05b2ce2d2c818c42787438346d08cc478491ed26
Sushidezain <136917380+Sushidezain@users.noreply.github.com>
2023-06-26 20:52:10 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -42,13 +42,16 @@ def main():
     else:
         st.markdown('<p style="font-size: 18px; font-weight: bold;">EP je rovno EP0</p>', unsafe_allow_html=True)
 
- # Plotting the line graph
-    fig, ax = plt.subplots()
-    ax.plot([EP0, EP], color='blue', marker='o')
-    ax.set_xlabel('EP0')
-    ax.set_ylabel('EP')
-    ax.set_title('Comparison between EP0 and EP')
-    st.pyplot(fig)
+# Plotting the line graph
+fig, ax = plt.subplots()
+percentage_change = ((EP - EP0) / EP0) * 100
+ax.plot([0, 1], [EP0, EP], color='blue', marker='o')
+ax.set_xticks([0, 1])
+ax.set_xticklabels(['EP0', 'EP'])
+ax.set_ylabel('Percentage Change')
+ax.set_title('Comparison between EP0 and EP (Percentage Change)')
+ax.annotate(f'{percentage_change:.2f}%', xy=(1, EP), xytext=(10, 0), textcoords='offset points')
+st.pyplot(fig)
 
 if __name__ == '__main__':
     main()
