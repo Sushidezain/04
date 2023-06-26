f83e3d0995a5e2b5badbb97479732e853a784dd5
Sushidezain <136917380+Sushidezain@users.noreply.github.com>
2023-06-26 20:15:47 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -18,13 +18,13 @@ def main():
 
     EP0 = st.number_input('EP0', value=10000000.0, format="%.0f", step=1000.0)
     IP1 = st.slider('IP1', min_value=120.0, max_value=150.0, step=0.1, value=120.0)
-    PM1 = st.slider('PM1', min_value=39000, max_value=50000, step=1000, value=40000)
+    PM1 = st.slider('PM1', min_value=39000, max_value=50000, step=100, value=40000)
 
     EP = calculate_EP(IP1, PM1, EP0)
     difference = EP - EP0
 
-    st.write('EP0: {:,}'.format(EP0).replace(',', ' '))
-    st.write('EP: {:,}'.format(EP).replace(',', ' '))
+    st.write('EP0 = {:,}'.format(EP0).replace(',', ' '))
+    st.write('EP = {:,}'.format(EP).replace(',', ' '))
     st.markdown('<h2 style="font-size: 24px;">Porovnání  mezi EP a EP0:</h2>', unsafe_allow_html=True)
     if EP > EP0:
         st.markdown('<p style="font-size: 18px; color: green; font-weight: bold;">EP je větší než EP0 o {:,}</p>'.format(int(difference)).replace(',', ' '), unsafe_allow_html=True)
