2e341a0ab99ef4b8891c5680f12cf89150b50107
Sushidezain <136917380+Sushidezain@users.noreply.github.com>
2023-06-26 20:57:04 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,21 +1,16 @@
 import streamlit as st
-import matplotlib.pyplot as plt
 import locale
+import matplotlib.pyplot as plt
 
-st.markdown('<h2 style="font-size: 40px;">Inflační doložka</h2>', unsafe_allow_html=True)
+st.markdown('<h2 style="font-size: 30px;">Inflační doložka</h2>', unsafe_allow_html=True)
 
 def calculate_EP(IP1, PM1, EP0):
     IP0 = 144.9
     PM0 = 41446
-    
-    st.markdown('<h2 style="font-size: 30px;">Vstupní data:</h2>', unsafe_allow_html=True)
-    
-    st.write('IP0 = {:,}'.format(IP0).replace(',', ' '))
-    st.write('PM0 = {:,}'.format(PM0).replace(',', ' '))
-    
+
     if IP0 == 0 or PM0 == 0:
         return IP0, PM0, EP0
-    
+
     EP = 0.8 * EP0 * (0.5 * (IP1 / IP0) + 0.5 * (PM1 / PM0)) + 0.2 * EP0
     return IP0, PM0, EP
 
@@ -24,13 +19,13 @@ def main():
     locale.setlocale(locale.LC_ALL, '')
 
     EP0 = st.number_input('EP0', value=10_000_000, step=1_000, format="%d")
-    IP1 = st.slider('IP1', min_value=130.0, max_value=160.0, step=0.1, value=144.9)
-    PM1 = st.slider('PM1', min_value=39000, max_value=50000, step=1, value=41446)
+    IP1 = st.slider('IP1', min_value=130.0, max_value=160.0, step=0.1, value=145.0)
+    PM1 = st.slider('PM1', min_value=39000, max_value=50000, step=100, value=40000)
 
     IP0, PM0, EP = calculate_EP(IP1, PM1, EP0)
     difference = EP - EP0
 
-    st.markdown('<h2 style="font-size: 30px;">Porovnání mezi EP a EP0:</h2>', unsafe_allow_html=True)
+    st.markdown('<h2 style="font-size: 24px;">Porovnání mezi EP a EP0:</h2>', unsafe_allow_html=True)
 
     st.write('EP0 = {:,}'.format(EP0).replace(',', ' '))
     st.write('EP = {:,}'.format(int(EP)).replace(',', ' '))
@@ -42,16 +37,16 @@ def main():
     else:
         st.markdown('<p style="font-size: 18px; font-weight: bold;">EP je rovno EP0</p>', unsafe_allow_html=True)
 
-# Plotting the line graph
-fig, ax = plt.subplots()
-percentage_change = ((EP - EP0) / EP0) * 100
-ax.plot([0, 1], [EP0, EP], color='blue', marker='o')
-ax.set_xticks([0, 1])
-ax.set_xticklabels(['EP0', 'EP'])
-ax.set_ylabel('Percentage Change')
-ax.set_title('Comparison between EP0 and EP (Percentage Change)')
-ax.annotate(f'{percentage_change:.2f}%', xy=(1, EP), xytext=(10, 0), textcoords='offset points')
-st.pyplot(fig)
+    # Plotting the line graph
+    fig, ax = plt.subplots()
+    percentage_change = ((EP - EP0) / EP0) * 100
+    ax.plot([0, 1], [EP0, EP], color='blue', marker='o')
+    ax.set_xticks([0, 1])
+    ax.set_xticklabels(['EP0', 'EP'])
+    ax.set_ylabel('Percentage Change')
+    ax.set_title('Comparison between EP0 and EP (Percentage Change)')
+    ax.annotate(f'{percentage_change:.2f}%', xy=(1, EP), xytext=(10, 0), textcoords='offset points')
+    st.pyplot(fig)
 
 if __name__ == '__main__':
     main()
